2f7306833d3da865be6aa76f3cd07ea3c1803eb2
Alvaro Vilanova Vidal <alvivi@gmail.com>
2021-05-03 12:16:48 +0200

    Stub confirm behaviour

--- lib/amqp_helpers/adapters/stub.ex
+++ lib/amqp_helpers/adapters/stub.ex
@@ -1,6 +1,10 @@
 defmodule AMQPHelpers.Adapters.Stub do
   @moduledoc """
-  TODO
+  A `AMQPHelpers.Adapter` which just logs.
+
+  This module implements a stub for `AMQPHelpers.Adapter` which mainly logs
+  calls. You probably want to use this for non-integration testing or a
+  development environment without a AMQP broker.
   """
 
   @behaviour AMQPHelpers.Adapter
@@ -28,7 +32,7 @@ defmodule AMQPHelpers.Adapters.Stub do
     conn_pid = Process.spawn(fn -> Process.sleep(:infinity) end, [])
     conn = %AMQP.Connection{pid: conn_pid}
 
-    chan_pid = Process.spawn(fn -> Process.sleep(:infinity) end, [])
+    {:ok, chan_pid} = Agent.start(fn -> %{confirm_handler: nil, delivery_tag: 1} end)
     chan = %AMQP.Channel{conn: conn, pid: chan_pid}
 
     {:ok, chan}
@@ -69,14 +73,14 @@ defmodule AMQPHelpers.Adapters.Stub do
   def publish(chan, exchange, routing_key, payload, options) do
     log(:publish, [chan, exchange, routing_key, payload, options])
 
-    # %{confirm_handler: confirm_handler, delivery_tag: delivery_tag} =
-    # Agent.get_and_update(chan.pid, fn state ->
-    # {state, %{state | delivery_tag: state.delivery_tag + 1}}
-    # end)
+    %{confirm_handler: confirm_handler, delivery_tag: delivery_tag} =
+      Agent.get_and_update(chan.pid, fn state ->
+        {state, %{state | delivery_tag: state.delivery_tag + 1}}
+      end)
 
-    # unless is_nil(confirm_handler) do
-    # send(confirm_handler, {:basic_ack, delivery_tag, false})
-    # end
+    unless is_nil(confirm_handler) do
+      send(confirm_handler, {:basic_ack, delivery_tag, false})
+    end
 
     :ok
   end
@@ -85,7 +89,7 @@ defmodule AMQPHelpers.Adapters.Stub do
   def register_confirm_handler(chan, handler) do
     log(:register_confirm_handler, [chan, handler])
 
-    # Agent.update(chan.pid, fn state -> %{state | confirm_handler: handler} end)
+    Agent.update(chan.pid, fn state -> %{state | confirm_handler: handler} end)
 
     :ok
   end
